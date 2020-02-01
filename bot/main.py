bf7288cee050306a38cc9a80980512dfeb770baf
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 13:38:17 +0100

    improve turning algorithm

--- bot/main.py
+++ bot/main.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     mc.change_speed_right(motor_speed)
     mpu = mpu6050.mpu6050(0x68)
 
+    last_z_turn = 0
     degree_turned = 0
     while degree_turned < TURN_DEGREE:
-        if degree_turned > (TURN_DEGREE - (TURN_DEGREE / 10)):
+        if degree_turned > (TURN_DEGREE - (TURN_DEGREE / 5)):
             print('Slowing down motors')
             motor_speed = 20
             mc.change_speed_left(motor_speed)
@@ -25,7 +26,13 @@ if __name__ == "__main__":
         mc.turn_left()
         time.sleep(SLEEP_TIME)
         gyro_z_scaled =  mpu.get_gyro_data()['z'] * SLEEP_TIME - SENSOR_DRIFT
+        last_z_turn = gyro_z_scaled
+        print('Last z turn:', last_z_turn)
         degree_turned += gyro_z_scaled
+        remaining_degree = TURN_DEGREE - degree_turned
+        print('Remaining degree:', remaining_degree)
+        remaining_seconds_to_turn = SLEEP_TIME / last_z_turn * remaining_degree
+        print('Predicted remaining seconds to turn:', remaining_seconds_to_turn)
         print('Turned: ', degree_turned)
 
     mc.turn_right()
