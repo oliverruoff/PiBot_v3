44d2f7f9ce58b0fe2b834e19b436b75a8f8f77e7
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 13:18:05 +0100

    improve turning algorithm

--- bot/main.py
+++ bot/main.py
@@ -8,19 +8,23 @@ from sensing import mpu6050
 if __name__ == "__main__":
     SENSOR_DRIFT = -1.8378
     SLEEP_TIME = 0.1
-    MOTOR_SPEED = 30
     TURN_DEGREE = 90
-    mc.change_speed_left(MOTOR_SPEED)
-    mc.change_speed_right(MOTOR_SPEED)
+
+    motor_speed = 30
+    mc.change_speed_left(motor_speed)
+    mc.change_speed_right(motor_speed)
+    mpu = mpu6050.mpu6050(0x68)
 
     degree_turned = 0
     while degree_turned < TURN_DEGREE:
+        if degree_turned > (TURN_DEGREE - (TURN_DEGREE / 10)):
+            motor_speed = 20
+            mc.change_speed_left(motor_speed)
+            mc.change_speed_right(motor_speed)
         mc.turn_left()
         time.sleep(SLEEP_TIME)
-        mpu = mpu6050.mpu6050(0x68)
-        gyro_data = mpu.get_gyro_data()
-        gyro_z = gyro_data['z']
-        gyro_z_scaled = gyro_z * SLEEP_TIME - SENSOR_DRIFT
+        gyro_z_scaled =  mpu.get_gyro_data()['z'] * SLEEP_TIME - SENSOR_DRIFT
         degree_turned += gyro_z_scaled
         print('Turned: ', degree_turned)
+
     mc.stop_motors()
