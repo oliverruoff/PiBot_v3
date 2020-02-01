38bbcddc679ca98079fb8fe40d9ad4b9ee2af16b
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 12:17:42 +0100

    added logic to turn 90 degree test

--- bot/main.py
+++ bot/main.py
@@ -1,40 +1,23 @@
 import math
+import time
 
 from movement import motor_controls as mc
-from usdar.usdar import detection as dt
-from usdar.usdar import ultrasonic as us
-from usdar.usdar import stepper as st
-
-
-def get_angle_to_turn(scan_speed, scan_turn_direction):
-    env_map = dt.scan_360(scan_speed, scan_turn_direction)
-    distances = [(x, y, math.sqrt(x*x+y*y)) for x, y in env_map]
-    max_dist = distances[[i[2]
-                          for i in distances].index(
-        max([i[2] for i in distances]))]
-    return math.degrees(math.atan2(max_dist[0], max_dist[1]))
-
-
-def reset_us_stepper(ticks_to_reset):
-    st.run_stepper(ticks_to_reset, False)
+from sensing import mpu6050
 
 
 if __name__ == "__main__":
-    turn_direction = True
-    while True:
-        if turn_direction:
-            turn_direction = False
-        else:
-            turn_direction = True
-        angle_to_turn = get_angle_to_turn(4, turn_direction)
-        if angle_to_turn < 0:
-            angle_to_turn = 360 + angle_to_turn
-        print('The most space seems to be at:', angle_to_turn, '°.')
-        mc.turn_degree(angle_to_turn)
-        front_dist = us.get_distance()
-        print('In front of me there is', front_dist, 'cm space.')
-        if front_dist > 40:
-            mc.move_front()
-            while front_dist > 40:
-                front_dist = us.get_distance()
-        mc.stop_motors()
+    SLEEP_TIME = 0.1
+    mc.change_speed_left(40)
+    mc.change_speed_right(40)
+    degree_turned = 0
+
+    while degree_turned < 90:
+        mc.turn_left()
+        time.sleep(SLEEP_TIME)
+        mpu = mpu6050.mpu6050(0x68)
+        gyro_data = mpu.get_gyro_data()
+        gyro_z = gyro_data['z']
+        gyro_z_scaled = gyro_z * SLEEP_TIME
+        degree_turned += gyro_z_scaled
+        print('Turned: ', degree_turned)
+    mc.stop_motors()
