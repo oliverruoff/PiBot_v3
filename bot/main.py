d946065501cdd602ca601c2925d3bdb7da5c1ab1
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 14:19:38 +0100

    moved to separat function

--- bot/main.py
+++ bot/main.py
@@ -5,11 +5,8 @@ from movement import motor_controls as mc
 from sensing import mpu6050
 
 
-if __name__ == "__main__":
-    SENSOR_DRIFT = -1.8378
+def gyro_turn(turn_degree, right=True, sensor_drift=-1.8378):
     SLEEP_TIME = 0.1
-    TURN_DEGREE = 180
-
     motor_speed = 50
     mc.change_speed_left(motor_speed)
     mc.change_speed_right(motor_speed)
@@ -17,20 +14,20 @@ if __name__ == "__main__":
 
     last_z_turn = 0
     degree_turned = 0
-    while degree_turned < TURN_DEGREE:
+    while degree_turned < turn_degree:
         print('---------------------')
-        if degree_turned > (TURN_DEGREE - (TURN_DEGREE / 5)):
+        if degree_turned > (turn_degree - (turn_degree / 5)):
             print('Slowing down motors')
             motor_speed = 20
             mc.change_speed_left(motor_speed)
             mc.change_speed_right(motor_speed)
         mc.turn_left()
         time.sleep(SLEEP_TIME)
-        gyro_z_scaled =  mpu.get_gyro_data()['z'] * SLEEP_TIME - SENSOR_DRIFT
+        gyro_z_scaled =  mpu.get_gyro_data()['z'] * SLEEP_TIME - sensor_drift
         last_z_turn = gyro_z_scaled
         print('Last z turn:', last_z_turn)
         degree_turned += gyro_z_scaled
-        remaining_degree = TURN_DEGREE - degree_turned
+        remaining_degree = turn_degree - degree_turned
         print('Remaining degree:', remaining_degree)
         remaining_seconds_to_turn = SLEEP_TIME / last_z_turn * remaining_degree
         print('Predicted remaining seconds to turn:', remaining_seconds_to_turn)
@@ -46,3 +43,6 @@ if __name__ == "__main__":
     mc.turn_right()
     time.sleep(0.1)
     mc.stop_motors()
+
+if __name__ == "__main__":
+    gyro_turn(90, True)
