06b08191ba4ff8e6f16f74fe12fa6676078839d9
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 13:45:13 +0100

    improving stopping for turning algorithm

--- bot/main.py
+++ bot/main.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     last_z_turn = 0
     degree_turned = 0
     while degree_turned < TURN_DEGREE:
+        print('---------------------')
         if degree_turned > (TURN_DEGREE - (TURN_DEGREE / 5)):
             print('Slowing down motors')
             motor_speed = 20
@@ -34,6 +35,11 @@ if __name__ == "__main__":
         remaining_seconds_to_turn = SLEEP_TIME / last_z_turn * remaining_degree
         print('Predicted remaining seconds to turn:', remaining_seconds_to_turn)
         print('Turned: ', degree_turned)
+        print('---------------------')
+        if remaining_seconds_to_turn < last_z_turn:
+            print('Detecting that I should stop, only continuing for ', remaining_seconds_to_turn, 'seconds!')
+            time.sleep(remaining_seconds_to_turn)
+            break
 
     mc.turn_right()
     time.sleep(0.1)
