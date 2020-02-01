091cc20283efe74a0df2dc2ccc09c3e9d9244501
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-01 12:41:00 +0100

    removed sensor drift

--- bot/main.py
+++ bot/main.py
@@ -6,18 +6,21 @@ from sensing import mpu6050
 
 
 if __name__ == "__main__":
+    SENSOR_DRIFT = -1.8378
     SLEEP_TIME = 0.1
-    mc.change_speed_left(40)
-    mc.change_speed_right(40)
-    degree_turned = 0
+    MOTOR_SPEED = 30
+    TURN_DEGREE = 90
+    mc.change_speed_left(MOTOR_SPEED)
+    mc.change_speed_right(MOTOR_SPEED)
 
-    while degree_turned < 90:
+    degree_turned = 0
+    while degree_turned < TURN_DEGREE:
         mc.turn_left()
         time.sleep(SLEEP_TIME)
         mpu = mpu6050.mpu6050(0x68)
         gyro_data = mpu.get_gyro_data()
         gyro_z = gyro_data['z']
-        gyro_z_scaled = gyro_z * SLEEP_TIME
+        gyro_z_scaled = gyro_z * SLEEP_TIME - SENSOR_DRIFT
         degree_turned += gyro_z_scaled
         print('Turned: ', degree_turned)
     mc.stop_motors()
