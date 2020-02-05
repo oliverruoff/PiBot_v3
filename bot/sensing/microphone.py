ac94d81459b9ce6c18f156dcc45409d8cb6265ef
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-05 21:59:56 +0100

    fix unnecessary in return of speech

--- bot/sensing/microphone.py
+++ bot/sensing/microphone.py
@@ -23,7 +23,7 @@ class microphone:
             # for testing purposes, we're just using the default API key
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
-            return "Google Speech Recognition thinks you said " + self.recognizer.recognize_google(audio)
+            return self.recognizer.recognize_google(audio)
         except sr.UnknownValueError:
             return "Google Speech Recognition could not understand audio"
         except sr.RequestError as e:
