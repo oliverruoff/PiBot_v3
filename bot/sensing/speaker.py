3bbcca0ce9fe903c8b25e50b57063675fbd53da5
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-09 22:38:29 +0100

    brought speaking to separate thread

--- bot/sensing/speaker.py
+++ bot/sensing/speaker.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 
 class speaker:
@@ -7,7 +8,13 @@ class speaker:
         os.system('mpg321 ' + path_to_audio + ' > /dev/null')
 
     def say_hi(self):
-        self.play_sound("assets/WALLE\\ 1.mp3")
+        thread = Thread(target=self.play_sound, args=(
+            "assets/WALLE\\ 1.mp3", )).start()
+        thread.start()
+        # self.play_sound("assets/WALLE\\ 1.mp3")
 
     def say_whoa(self):
-        self.play_sound("assets/Whoa.mp3")
+        thread = Thread(target=self.play_sound, args=(
+            "assets/Whoa.mp3", )).start()
+        thread.start()
+        # self.play_sound("assets/Whoa.mp3")
