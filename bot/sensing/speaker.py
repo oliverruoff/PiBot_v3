fa29310007a8bc642e037791e3fde63a3d0dc48b
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-09 21:00:07 +0100

    changed sound playing

--- bot/sensing/speaker.py
+++ bot/sensing/speaker.py
@@ -1,14 +1,10 @@
-import pygame
+import os
 
 
 class speaker:
 
     def play_sound(self, path_to_audio):
-        pygame.mixer.init()
-        pygame.mixer.music.load(path_to_audio)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() is True:
-            continue
+        os.system('mpg321 ' + path_to_audio + ' &')
 
     def say_hi(self):
         self.play_sound("/home/pi/develop/PiBot_v3/bot/assets/WALLE 1.mp3")
