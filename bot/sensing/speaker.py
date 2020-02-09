3103d63afbacccf02810aa609e98fbdd5e8a7706
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-09 21:05:52 +0100

    switched sounds to subprocess

--- bot/sensing/speaker.py
+++ bot/sensing/speaker.py
@@ -1,10 +1,12 @@
-import os
+from subprocess import DEVNULL, STDOUT, check_call
 
 
 class speaker:
 
     def play_sound(self, path_to_audio):
-        os.system('mpg321 ' + path_to_audio + ' > /dev/null')
+        check_call(['mpg321', path_to_audio],
+                   stdout=DEVNULL, stderr=STDOUT)
+        # os.system('mpg321 ' + path_to_audio + ' > /dev/null')
 
     def say_hi(self):
         self.play_sound("assets/WALLE\\ 1.mp3")
