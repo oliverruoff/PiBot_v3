ee8fbe05205d98e46564ad760c2365eb66136725
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-09 21:13:23 +0100

    switched sound back to os.system

--- bot/sensing/speaker.py
+++ bot/sensing/speaker.py
@@ -1,12 +1,10 @@
-from subprocess import DEVNULL, STDOUT, check_call
+import os
 
 
 class speaker:
 
     def play_sound(self, path_to_audio):
-        check_call(['mpg321 ' + path_to_audio],
-                   stdout=DEVNULL, stderr=STDOUT)
-        # os.system('mpg321 ' + path_to_audio + ' > /dev/null')
+        os.system('mpg321 ' + path_to_audio + ' > /dev/null')
 
     def say_hi(self):
         self.play_sound("assets/WALLE\\ 1.mp3")
