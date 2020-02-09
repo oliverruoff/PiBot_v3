8a2a2d0ac3291da22e18231761b768f216df619a
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-09 21:08:34 +0100

    fix sound call

--- bot/sensing/speaker.py
+++ bot/sensing/speaker.py
@@ -4,7 +4,7 @@ from subprocess import DEVNULL, STDOUT, check_call
 class speaker:
 
     def play_sound(self, path_to_audio):
-        check_call(['mpg321', path_to_audio],
+        check_call(['mpg321 ' + path_to_audio],
                    stdout=DEVNULL, stderr=STDOUT)
         # os.system('mpg321 ' + path_to_audio + ' > /dev/null')
 
