ac57c65cea3c4ec5c2323397f646b61dc5bd7e1d
Oliver Ruoff <oliver93ruoff@web.de>
2020-02-28 20:22:41 +0100

    testing object detection

--- bot/Robot.py
+++ bot/Robot.py
@@ -232,7 +232,9 @@ class Robot():
                 print('No recognized command! ->', spoken_words)
 
     def test(self):
-        self.gyro_turn(720)
+        for _ in range(5):
+            self.camera.detect_objects_v2()
+            self.gyro_turn(72)
 
     def _test(self):
         self.gyro_move_start(forward=True)
