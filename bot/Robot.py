c48d0c978437f8b0304de1f724b6b86c3b6f8ef4
Oliver Ruoff <oliver93ruoff@web.de>
2020-03-01 13:24:34 +0100

    switching turning

--- bot/Robot.py
+++ bot/Robot.py
@@ -242,7 +242,7 @@ class Robot():
     def test(self):
         x_diff, box_img_ratio = self.turn_look_for_object('person')
         while abs(x_diff) > 5:
-            right = True if x_diff > 0 else False
+            right = True if x_diff < 0 else False
             self.gyro_turn(abs(x_diff), right, motor_speed=50)
             x_diff, box_img_ratio = self.turn_look_for_object('person')
             if x_diff == 0:
